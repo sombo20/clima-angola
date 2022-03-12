2a6b2eaead2a0012ef2bca71a32b77e460dce7d0
VICENTE <64757262+sombo20@users.noreply.github.com>
2022-03-12 20:47:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function menu(){
+			  var menu = document.querySelector("nav")
+			  menu.style.top = "0px"
+			  menu.style.transition ="200ms"
+			}
+			
+			function sair(){
+			var menu = document.querySelector("nav")
+			menu.style.top = "-300px"
+			menu.style.transition ="200ms"
+			}
